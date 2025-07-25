3c02e5a33f5f9a1651f4f033eb6df8afe76a02b2
Saeyeon Lim <106756920+saeyeonn@users.noreply.github.com>
2025-07-25 16:11:32 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 15.42 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,47 +1,13 @@
 import java.util.*;
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        ArrayList<int[]> dataList = new ArrayList<>();
-        int extIndex = switch (ext) {
-            case "date" -> 1;
-            case "maximum" -> 2;
-            case "remain" -> 3;
-            default -> 0;
-        };
+        List<String> columnList = new ArrayList<>(Arrays.asList("code", "date", "maximum", "remain"));
+        int extIndex = columnList.indexOf(ext);
+        int sortIndex = columnList.indexOf(sort_by);
 
-        int sortIndex = switch (sort_by) {
-            case "date" -> 1;
-            case "maximum" -> 2;
-            case "remain" -> 3;
-            default -> 0;
-        };
-
-        for (int[] datum : data) {
-            if (datum[extIndex] < val_ext) {
-                dataList.add(datum);
-            }
-        }
-
-        int[][] answer = new int[dataList.size()][4];
-        int i = 0;
-        for (int[] datum : dataList) {
-            answer[i++] = datum;
-        }
-
-        System.out.println(extIndex);
-
-        for (i = 0; i < answer.length - 1; i++) {
-            System.out.println(answer[i][extIndex]);
-
-            for (int j = i + 1; j < answer.length; j++) {
-                if (answer[i][sortIndex] > answer[j][sortIndex]) {
-                    int[] temp = answer[i];
-                    answer[i] = answer[j];
-                    answer[j] = temp;
-                }
-            }
-        }
-
-        return answer;
+        return Arrays.stream(data)
+                .filter(content -> content[extIndex] < val_ext)
+                .sorted(Comparator.comparingInt(c -> c[sortIndex]))
+                .toArray(int[][]::new);
     }
 }
